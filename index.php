94b5db6681ed793e4127519ce22f7f35f98a8d05
Edwin Aquino <github@edwinaquino.com>
2025-11-23 21:49:22 -0600

    adde acr user to main.yml

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tech with Muk</title>
+    <title>Azure + Github Actions + Docker</title>
     <style>
         body {
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
@@ -21,5 +21,6 @@
 </head>
 <body>
     <h1>Hi this is Azure GIthub Actions and Docker Container</h1>
+    <p>947pm: added ACR USER AND PASSWORD:</p>
 </body>
 </html>
